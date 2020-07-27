e044eaf281f8fd4b54fd17e2309422cd14fe0a5c
deyvisonpenha <deyvisonpenha1@gmail.com>
2020-07-27 00:18:42 +0000

    fix update user

--- src/services/UpdateUserService.ts
+++ src/services/UpdateUserService.ts
@@ -1,4 +1,4 @@
-import { getRepository, getConnection, getMongoRepository, MongoRepository} from 'typeorm';
+import { getRepository, getConnection, getMongoRepository, MongoRepository } from 'typeorm';
 import User from '../models/User';
 import { hash } from 'bcryptjs';
 
@@ -9,34 +9,26 @@ class UpdateUserService {
     this.ormRepository = getMongoRepository(User);
   }
 
-  public async execute(id, userParamsUpdate): Promise< User | undefined >{
-    try{
-      /*
-      const response = await getConnection()
-      .createQueryBuilder()
-      .update(User)
-      .set(userParamsUpdate)
-      .where("id = :id", { id })
-      .execute();
+  public async execute(id, userParamsUpdate): Promise<User | undefined> {
+    try {
+      var ObjectID = require('mongodb').ObjectID;
 
-      console.log(response);
+      if (userParamsUpdate.password) {
+        delete userParamsUpdate.password;
+      }
 
-      const userRepository = getMongoRepository(User);
+      const response = await this.ormRepository.findOneAndUpdate(
+        { _id: ObjectID(id) },
+        { $set: userParamsUpdate },
+        { returnOriginal: false },
+      );
 
-      const user = await userRepository.findOne({where: {id}});
-      */
-     var ObjectID = require('mongodb').ObjectID;
-     const response = await this.ormRepository.findOneAndUpdate(
-      {_id: ObjectID(id)},
-      { $set: userParamsUpdate},
-      {returnOriginal: false },
-    );
-    const updatedUser = response.value;
+      const updatedUser = response.value;
 
       delete updatedUser?.password
 
       return updatedUser;
-    }catch{
+    } catch{
       throw new Error('Error Update User')
     }
   }
