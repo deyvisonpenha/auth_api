3b03a3b4b901c4204183140fa80e55375bf3f7b1
Deyvison Penha <deyvisonpenha1@gmail.com>
2020-08-23 13:09:23 +0000

    add postgres database

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = [{
   "name": "default",
   "type": "mongodb",
   "host": "localhost",
@@ -11,4 +11,17 @@ module.exports = {
   "cli": {
     "migrationsDir": "./src/database/migrations/"
   }
+},
+{
+  "name": "postgres",
+  "type": "postgres",
+  "url": "postgres://gitpod@127.0.0.1",
+  "database": "pedeae_new_api",
+  "port": 5432,
+  "entities": ["./src/models/*.{ts,js}"],
+  "migrations": ["./src/database/migrations/*.{ts,js}"],
+  "cli": {
+    "migrationsDir": "./src/database/migrations/"
+  }
 }
+]
